209c172d7a9e8daeced877bb6b113155a6e5066a
Jaosou <89335399+Jaosou@users.noreply.github.com>
2026-06-25 19:04:18 +0700

    Create netify config for deploy

--- apps/letter-luv-app/next.config.js
+++ apps/letter-luv-app/next.config.js
@@ -1,22 +1,55 @@
-//@ts-check
+// //@ts-check
+
+// /** @type {import('next').NextConfig} */
+//   images: {
+//     remotePatterns: [{
+//       protocol: 'http',
+//       hostname: 'localhost',
+//       port: '8888',
+//       pathname: 'assets/uploads/**'
+//     },
+//     new URL('http://localhost:8888/show-image/**')]
+//   },
+//   allowedDevOrigins: [
+//     'https://outline-provided-sequence.ngrok-free.dev',
+//   ],
+//   // Next.js options go here
+//   // See: https://nextjs.org/docs/app/api-reference/config/next-config-js
+// };
+
+// module.exports = nextConfig;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [{
-      protocol: 'http',
-      hostname: 'localhost',
-      port: '8888',
-      pathname: 'assets/uploads/**'
-    },
-    new URL('http://localhost:8888/show-image/**')]
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8888/:path*',
+      },
+    ];
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8888',
+        pathname: '/assets/uploads/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8888',
+        pathname: '/show-image/**',
+      },
+    ],
+  },
+
   allowedDevOrigins: [
     'https://outline-provided-sequence.ngrok-free.dev',
   ],
-  // Next.js options go here
-  // See: https://nextjs.org/docs/app/api-reference/config/next-config-js
 };
 
 module.exports = nextConfig;
-
